14c73354f33f0aaa272509daad80d7db470fb024
unknown <tech@shipyaari.com>
2022-09-07 17:27:58 +0530

    designr

--- cart/cart.js
+++ cart/cart.js
@@ -1,46 +1,32 @@
-var obj = [{
-        foodname: 'icecream',
-        qty: 1,
-        price: 200
-    },
-    {
-        foodname: 'thali',
-        qty: 1,
-        price: 300
-    },
-    {
-        foodname: 'Panipuri',
-        qty: 1,
-        price: 50
-    }
-]
-
-showcartdata(obj)
+var itemdta =JSON.parse(localStorage.getItem('cart'))||[]
 var addData = JSON.parse(localStorage.getItem('address')) || []
+showcartdata(itemdta)
 // if(addData.length>=3){
 //     document.getElementById('addnewadd').disabled = true
 // }else{
 //     document.getElementById('addnewadd').disabled = false
 // }
 showAddress()
-
 function showcartdata(obj) {
     obj.map(function (ele, index) {
         var div = document.createElement('div');
         div.setAttribute('class', 'flex1');
         var p = document.createElement('p');
-        p.innerText = ele.foodname;
+        p.innerText = ele.itemName;
+        var img = document.createElement('img')
+        img.src=ele.itemImage
+        p.append(img)
         var div1 = document.createElement('div')
         div1.setAttribute('class', 'qtychange')
         var btn1 = document.createElement('button')
         btn1.innerText = '+'
         var span = document.createElement('span')
-        span.innerText = ele.qty
+        span.innerText = 1
         var btn2 = document.createElement('button')
         btn2.innerText = '-'
         div1.append(btn1, span, btn2)
         var p2 = document.createElement('p')
-        p2.innerText = ele.price
+        p2.innerText = ele.itemPrice
         div.append(p, div1, p2)
         setTimeout(() => {
             document.getElementById('right').append(div)
